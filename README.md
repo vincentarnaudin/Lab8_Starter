18b68102ae78a337644a50db226d8039cbc6f667
Vincent <arnaudinvi@cy-tech.fr>
2023-05-29 00:41:23 -0700

    lab done

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Lab 8 - Starter
+
+1- Where would you fit your automated tests in your Recipe project development pipeline?
+
+  Automated tests should be fitted within a GitHub action that runs whenever code is pushed to enable continuous integration, early issue detection, standardized testing environment, automation, efficiency, and integration with pull requests.
+
+2- Would you use an end to end test to check if a function is returning the correct output? (yes/no)
+
+  No. End-to-end tests are not typically used to check if a single function is returning the correct output.
+
+3- Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+  No, unit tests alone would not be sufficient to test the "message" feature of a messaging application as it requires testing the integration of multiple components, real-time aspects, user interactions, and performance, which are better addressed through integration tests, end-to-end tests, and other specialized testing approaches.
+
+4- Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+  Yes, a unit test would be suitable for testing the "max message length" feature of a messaging application as it involves a specific and isolated behavior within a single component, making it easily testable in isolation without dependencies on other components or external factors.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,4 +2,91 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
+// TODO - Part 2const functions = require('../code-to-unit-test/unit-test-me');
+
+describe('Unit tests', () => {
+  // isPhoneNumber tests
+  test('Valid phone number should return true', () => {
+    expect(functions.isPhoneNumber('123-456-7890')).toBe(true);
+  });
+
+  test("isPhoneNumber", () => {
+    expect(functions.isPhoneNumber("1234567890")).toBe(false);
+  });
+
+  test('Valid phone number should return true', () => {
+    expect(functions.isPhoneNumber('(123) 456-7890')).toBe(true);
+  });
+
+  test('Invalid phone number should return false', () => {
+    expect(functions.isPhoneNumber('123-45-6789')).toBe(false);
+  });
+
+  // isEmail tests
+  test('Valid email should return true', () => {
+    expect(functions.isEmail('test@example.com')).toBe(true);
+  });
+
+  test('Invalid email should return false', () => {
+    expect(functions.isEmail('testexample.com')).toBe(false);
+  });
+
+  test('Valid email should return true', () => {
+    expect(functions.isEmail('user_123@example.co')).toBe(true);
+  });
+
+  test('Invalid email should return false', () => {
+    expect(functions.isEmail('user@example')).toBe(false);
+  });
+
+  // isStrongPassword tests
+  test('Valid strong password should return true', () => {
+    expect(functions.isStrongPassword('password123_')).toBe(true);
+  });
+
+  test('Invalid strong password should return false', () => {
+    expect(functions.isStrongPassword('a')).toBe(false);
+  });
+
+  test('Valid strong password should return true', () => {
+    expect(functions.isStrongPassword('Abc1234_')).toBe(true);
+  });
+
+  test('Invalid strong password should return false', () => {
+    expect(functions.isStrongPassword('example123_123456789')).toBe(false);
+  });
+
+  // isDate tests
+  test('Valid date should return true', () => {
+    expect(functions.isDate('05/28/2023')).toBe(true);
+  });
+
+  test('Invalid date should return false', () => {
+    expect(functions.isDate('13/32/20')).toBe(false);
+  });
+
+  test('Valid date should return true', () => {
+    expect(functions.isDate('12/01/2022')).toBe(true);
+  });
+
+  test('Invalid date should return false', () => {
+    expect(functions.isDate('2022/12/01')).toBe(false);
+  });
+
+  // isHexColor tests
+  test('Valid hex color should return true', () => {
+    expect(functions.isHexColor('#FF0000')).toBe(true);
+  });
+
+  test('Invalid hex color should return false', () => {
+    expect(functions.isHexColor('#ZZZZZZ')).toBe(false);
+  });
+
+  test('Valid hex color should return true', () => {
+    expect(functions.isHexColor('#abcdef')).toBe(true);
+  });
+
+  test('Invalid hex color should return false', () => {
+    expect(functions.isHexColor('invalid')).toBe(false);
+  });
+});
